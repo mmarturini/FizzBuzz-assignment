e2a97a081994065081164ba753524bf0feef62ff
mmarturi <m.marturini22@gmail.com>
2021-11-14 11:58:24 +0100

    'implementation of FizzBuzz'

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,40 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
+
+        String aux;
+        for (int i=0; i < 106; i++) {
+
+            if ( i%3 == 0) {
+               if ( i%5 == 0) {
+                   if ( i%7 == 0) {
+                       aux = "FizzBuzzBang";
+                   }
+                   else {
+                       aux = "FizzBuzz";
+                   }
+               }
+               else {
+                   aux = "Fizz";
+               }
+           }
+           else if ( i%5 == 0) {
+               if ( i%7 == 0) {
+                   aux = "BuzzBang";
+               }
+               else {
+                   aux = "Buzz";
+               }
+           }
+           else if ( i%7 == 0) {
+               aux = "Bang";
+           }
+           else {
+               aux = String.valueOf(i);
+           }
+
+            System.out.println(aux);
+        }
         
     }
 
