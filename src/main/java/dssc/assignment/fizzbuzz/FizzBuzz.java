3487779c75a1f71eb31e1befaaf7a635568a3c43
mmarturi <m.marturini22@gmail.com>
2021-11-17 18:00:15 +0100

    '1 to 100 fizzbuzzes'

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,30 @@
 package dssc.assignment.fizzbuzz;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class FizzBuzz {
 
     public String convert(int number) {
-        return String.valueOf(number);
+
+        if ( number%15 ==0 ) {
+            return "FizzBuzz";
+        }
+        if ( number%5 == 0 ) {
+            return "Buzz";
+        }
+        if ( number%3 == 0 ) {
+            return "Fizz";
+        }
+        else {
+            return String.valueOf(number);
+        }
+    }
+
+    public void print() {
+        Stream<String> fizzBuzzes = IntStream.range(1, 101).mapToObj(this::convert);
+        System.out.println(fizzBuzzes.collect(Collectors.joining(", ")));
     }
 
     public static void main(String... args) {
--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class Convert {
 
     private final FizzBuzz fizzBuzz = new FizzBuzz();
- /*
+    // to run each test, junit create a new instance of the convert class
+    // that is why we can make fizzBuzz as this private method
+    // each test will have its own fizzBuzz to run
+
+    /*
     @Test
     void number_1_is_1() {
         int one = 1;
@@ -32,12 +37,12 @@ public class Convert {
     void number_40() {
         Assertions.assertEquals("40", fizzBuzz.convert(40));
     }
-  */
+    */
 
     // Parametrized tests make it possible to run same tests
     // with multiple arguments
     @ParameterizedTest
-    @CsvSource({"1, 1", "2, 2", "40, 40"})
+    @CsvSource({"1, 1", "2, 2", "41, 41"})
     void numbers_not_divisible_by_3_or_5_to_Strings(String expected, int number) {
         Assertions.assertEquals(expected, fizzBuzz.convert(number));
     }
@@ -46,6 +51,42 @@ public class Convert {
     // CsvSource
     // we have 3 pairs of arguments to run the test in CsvSource, and j unit
     // is using the first of the pair as string and the second as int to pass as arg to the test
+
+    /*
+    @Test
+    void number_3_to_Fizz() {
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
+    }
+
+    @Test
+    void number_6_to_Fizz() {
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(6));
+    }
+
+    @Test
+    void number_24_to_Fizz() {
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(24));
+    }
+    */
+
+    @ParameterizedTest
+    @ValueSource(ints = {3,6,24})
+    void multiple_of_3_to_Fizz(int number) {
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(number));
+    }
+    // @ValueSoruce cause we only have 1 argument
+
+    @ParameterizedTest
+    @ValueSource(ints = {10,20})
+    void multiple_of_5_to_Buzz(int number) {
+        Assertions.assertEquals("Buzz", fizzBuzz.convert(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {15,30,45})
+    void multiple_of_3_and_5_to_Fizz_Buzz(int number) {
+        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(number));
+    }
 }
 
 
